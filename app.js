a8891246fc7a826e67b4d6dd73fd1cbf6286553d
LOPEZ JARBY <jarbylopez2016q@gmail.com>
2025-08-14 23:10:00 -0600

    agregando comentario

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombreUsuario = prompt('Digite el nombre de sus amigos');
