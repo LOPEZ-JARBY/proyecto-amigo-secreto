8fe7dcef110bb0c24bdc1a4f6cd392752f7f75a9
LOPEZ JARBY <jarbylopez2016q@gmail.com>
2025-08-15 00:01:44 -0600

    agregar variable

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let nombreSecreto = "";
 let nombreUsuario = prompt('Digite el nombre de sus amigos');
+
 console.log(nombreUsuario);
